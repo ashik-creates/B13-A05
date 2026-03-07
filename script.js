eec94f303d3ff6ce05a0066972df388f7881b545
ashikur rahman <ashikorrahman8900@gmail.com>
2026-03-07 20:56:25 +0600

    added dynamic data on cards

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let allIssues = [];
 
+const showArrElement = (arr) => {
+  const newArr = arr.map(
+    (item) => `<div class="badge badge-sm text-sm ${
+      item.toLowerCase() === "bug"
+        ? "high"
+        : item.toLowerCase() === "help wanted"
+          ? "medium"
+          : item.toLowerCase() === "enhancement"
+            ? "next-eh"
+            : item.toLowerCase() === "documentation"
+              ? "low"
+              : "next-la"
+    } ">
+      ${item.toUpperCase()}
+    </div>`,
+  );
+  return newArr.join(" ");
+};
+
 document.getElementById("tabs-container").addEventListener("click", (event) => {
   const clickedBtn = event.target.closest(".tabs-btn");
 
@@ -76,43 +95,44 @@ function displayAllIssues(issues) {
   issues.forEach((item) => {
     const newCard = document.createElement("div");
     newCard.innerHTML = `
-    <div class="shadow-lg border border-gray-100 ${item.status === "open" ? "high-border-top" : "low-border-top"} space-y-4">
+    <div class="shadow-lg border border-gray-100 ${item.status === "open" ? "high-border-top" : "low-border-top"} space-y-6 h-full hover:shadow-2xl hover:-translate-y-1 cursor-pointer transition-all duration-300">
                <div class="card-upper p-3">
                 <div class="card-head flex justify-between">
                   <div>
                       <img ${item.status === "open" ? "src='./assets/Open-Status.png'" : "src='./assets/Closed-Status.png'"} alt="">
                   </div>
-                  <div class="badge badge-lg high">
-                      HIGH
+                  <div class="badge badge-lg ${
+                    item.priority === "high"
+                      ? "high"
+                      : item.priority === "low"
+                        ? "low"
+                        : "medium"
+                  }">
+                      ${item.priority.toUpperCase()}
                   </div>
                 
                 </div> 
-                <div class="card-middle space-y-2">
+                <div class="card-middle space-y-4">
                     <div class="pt-2 space-y-1">
                         <h2 class="font-semibold text-lg">
-                            Fix navigation menu on mobile devices
+                            ${item.title}
                         </h2>
                         <p class="text-sm text-gray-500 line-clamp-2">
-                            The navigation menu doesn't collapse properly on mobile devices
+                            ${item.description}
                         </p>
                     </div>
-                    <div id="card-badges" class="flex justify-start gap-1">
-                        <div class="badge badge-lg ">
-                            Bug
-                        </div>
-                        <div class="badge badge-lg ">
-                            Help wanted
-                        </div>
+                    <div id="card-badges" class="flex justify-start items-center flex-wrap gap-1">
+                        ${showArrElement(item.labels)}
                     </div>
                 </div>
                </div>
                 <hr class="text-gray-200">
                 <div class="card-lower p-3">
                   <p class="text-gray-500">
-                    #1 by john_doe
+                    #1 by ${item.author}
                   </p>
                   <p class="text-gray-500">
-                    1/15/2024
+                    ${new Date(item.createdAt).toLocaleDateString("en-US")}
                   </p>
                 </div>
             </div>
